920860865b57276a088cd63c65210b5070ee6e00
Adilson Angelo <aasj2@cin.ufpe.br>
2018-05-12 01:49:27 -0300

    Commit inicial

---
+++ dfsa/src/main/java/br/ufpe/cin/if740/dfsa/App.java
@@ -0,0 +1,13 @@
+package br.ufpe.cin.if740.dfsa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
